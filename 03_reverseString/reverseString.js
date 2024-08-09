72d0ad54f30ffdedf128a3ae64c3b8a9bb1304fb
justinmhudson <jhud2010@gmail.com>
2024-08-08 23:54:04 -0700

    Complete Lesson 03 Reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = userString => {
+    let reversedString = '';
+    let numberOfChars = userString.length;
+    for(let i = numberOfChars; i > 0; i--) {
+        let lastChar = userString[i - 1];
+        reversedString = reversedString.concat(lastChar);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line

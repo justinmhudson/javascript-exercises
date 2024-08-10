3e8693327496b0fb191898dac437065cbe18910f
justinmhudson <jhud2010@gmail.com>
2024-08-10 01:33:26 -0700

    Begin Lesson 04 'Remove From Array'
    Passed first test, with one argument. Need to troubleshoot use of
    ...args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = (array, ...items) => {
+    for (const item of items) {
+        let itemIndex = array.indexOf(item);
+        if (item >= 0) {
+            let removedItem = array.splice(itemIndex, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

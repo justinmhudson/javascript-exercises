81746050ccc815c2e1766a3229694fa21013517e
justinmhudson <jhud2010@gmail.com>
2024-08-11 14:13:35 -0700

    Complete Lesson 05 'Sum All'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    inputArray = [a, b];
+    let errorMessage = '';
+    let sum = 0;
 
+    let smallerNumber = Math.min(...inputArray);
+    let largerNumber = Math.max(...inputArray);
+    
+    inputArray.forEach(element => {
+        if(typeof element !== 'number' || element < 0 || element % 1 !== 0) {
+            errorMessage = 'ERROR';
+        }
+    });
+
+    if (errorMessage === 'ERROR') {
+        return errorMessage;
+    } else {
+        for(let i = smallerNumber; i <= largerNumber; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line

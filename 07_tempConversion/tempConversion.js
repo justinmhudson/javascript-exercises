5b8d0b9d3623e4cc6fc267f7267567e77853b424
justinmhudson <jhud2010@gmail.com>
2024-08-11 18:52:16 -0700

    Complete Lesson 07 'Temp Coversion'
    Wrote both as one-line arrow function and multiline function to make
    more readable.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+//const convertToCelsius = temp => Math.round((temp - 32) * 5 / 9 * 10) / 10;
+
+const convertToCelsius = function(temp) {
+  let tempInCelcius = (temp - 32) * 5 / 9;
+  let roundedTemp = Math.round(tempInCelcius * 10) / 10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+//const convertToFahrenheit = temp => Math.round((temp * 9 / 5 + 32) * 10) / 10;
+
+const convertToFahrenheit = function(temp) {
+  let tempInFahrenheit = temp * 9 / 5 + 32;
+  let roundedTemp = Math.round(tempInFahrenheit * 10) / 10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
